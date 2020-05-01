b6341e483f57cd534dc8dcb456e4c4c6cca9e646
Tishchenko Yevhen <tishchenk@gmail.com>
2020-05-01 12:37:33 +0300

    configure swagger

--- BookStore-API/Controllers/HomeController.cs
+++ BookStore-API/Controllers/HomeController.cs
@@ -6,14 +6,52 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore_API.Controllers
-{
+{/// <summary>
+/// This is a test API controller
+/// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class HomeController : ControllerBase
     {
-        public IActionResult Index() 
+        /// <summary>
+        /// Get values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/Home
+        [HttpGet]
+        public IEnumerable<string> Get()
         {
-            return Ok("Hello world");
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get a value
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET: api/Home/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Home
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Home/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }

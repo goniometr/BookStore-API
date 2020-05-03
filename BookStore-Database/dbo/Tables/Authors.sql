189ca2a46c6c0e3b4b1ef93033d561c437ea097d
Tishchenko Yevhen <tishchenk@gmail.com>
2020-05-03 19:31:07 +0300

    Create Database2

---
+++ BookStore-Database/dbo/Tables/Authors.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Authors] (
+    [id]        INT            IDENTITY (1, 1) NOT NULL,
+    [Firstname] NVARCHAR (50)  NULL,
+    [Lastname]  NVARCHAR (50)  NULL,
+    [Bio]       NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Authors] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ BookStore-Database/dbo/Tables/Books.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Books] (
+    [id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (100) NULL,
+    [Year]     INT            NULL,
+    [ISBN]     NVARCHAR (50)  NULL,
+    [Summary]  NVARCHAR (150) NULL,
+    [Image]    NVARCHAR (150) NULL,
+    [Price]    MONEY          NULL,
+    [AuthorId] INT            NULL,
+    CONSTRAINT [PK_Books] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_Books_Authors] FOREIGN KEY ([AuthorId]) REFERENCES [dbo].[Authors] ([id])
+);
+
